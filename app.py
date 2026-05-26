8fd95f279824b292dd7f72ab9c1cd50002165777
Nicolas René Ledard <n.r.ledard@gmail.com>
2026-05-26 03:52:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11753,6 +11753,8 @@ if not result:
     st.stop()
 
 
+
+
 # =============================
 # DEBUG — Savoir vs pénalités
 # =============================
@@ -11786,7 +11788,7 @@ def debug_knowledge_balance(result: dict):
         "Ce debug sert à vérifier si un texte riche en savoir explicite est trop pénalisé "
         "par scientificité rhétorique, autorité vague, frame shift ou ancrage réel fragile."
     )
-
+debug_knowledge_balance(result)
 
 
     
